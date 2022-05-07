9607e08e9e9c3e80be611411413963863de4f341
Sergey Grechko <sergey.grechko@hys-enterprise.com>
2022-05-07 22:18:32 +0300

    The initial commit: let the Time begin.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () =>
+{
+    return "CHRONOS: the God of Time.";
+});
+
+app.Run();
