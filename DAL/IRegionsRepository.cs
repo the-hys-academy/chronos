2a379440624f6106c0710ae59a44724c12f19a99
fan1ech00 <fan1ech000@tuta.io>
2022-06-13 16:31:45 +0300

    Set universal type(T) in "regions repository"

--- DAL/IRegionsRepository.cs
+++ DAL/IRegionsRepository.cs
@@ -3,8 +3,10 @@ using chronos.DAL.Models;
 
 namespace chronos.DAL;
 
-public interface IRegionsRepository: IGetRepository<Region>, 
-    IGetAllRepository<Region>, ICreateRepository<Region>, IUpdateRepository<Region>, IRemoveRepository
+using T = Region;
+
+public interface IRegionsRepository: IGetRepository<T>, 
+    IGetAllRepository<T>, ICreateRepository<T>, IUpdateRepository<T>, IRemoveRepository
 {
     
 }
